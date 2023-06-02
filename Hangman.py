4fe31648db35fd57bdfc3aecfbfe271d41be6d36
alra0351 <alexejrausch97@gmail.com>
2023-06-02 18:06:58 +0200

    initial commit

---
+++ Hangman.py
@@ -0,0 +1,100 @@
+import random
+import os
+
+def clear_screen():
+    os.system('cls')
+    print("Bildschirm geleert")
+
+def main_menu():
+    print("(1) Neues Spiel starten")
+    print("(2) Statistiken einsehen")
+    print("(3) Statistiken löschen")
+    print("(4) Beenden")
+    user_choice = input()
+    if user_choice == "1":
+        return new_game()
+    elif user_choice == "2":
+        see_stats()
+    elif user_choice == "3":
+        del_stats()
+    elif user_choice == "4":
+        end_game()
+    else:
+        print("Wrong Input. Choose again")
+        main_menu()
+        
+        
+def new_game():
+        
+    with open("Wortliste.txt", "r") as wordlist:
+        lines = wordlist.read().splitlines()
+        random_word = random.choice(lines)
+    game_string = len(random_word) * "_"
+    number_lifes = 5
+    return game(random_word, game_string, number_lifes)
+
+
+
+def game(random_word, game_string, number_lifes):
+    while( (random_word != game_string) & (number_lifes > 0) ):
+        clear_screen()
+        print("Noch %d Leben übrig" % number_lifes)
+        print(game_string)
+        letter_input(random_word, game_string, number_lifes)
+    if number_lifes > 0:
+        print("Du hast das Spiel gewonnen.")
+    else:
+        print("Du hast das Spiel verloren")
+        return 0
+          
+         
+def letter_input(random_word, game_string, number_lifes):
+    letter = input()
+    correct_letters = 0
+    word = random_word
+    while letter in word:
+        letter_pos = word.index(letter)
+        list_game_string = list(game_string)
+        list_game_string[letter_pos] = letter
+        game_string = ""
+        for i in list_game_string:
+            game_string += i
+        list_word = list(word)
+        list_word[letter_pos] = " "
+        word = ""
+        for i in list_word:
+            word += i
+        correct_letters += 1
+    if correct_letters > 0:
+        pass
+    else:
+        number_lifes -= 1
+    
+    
+def see_stats():
+    return 0
+
+def del_stats():
+    return 0
+
+def end_game():
+    return 0
+
+clear_screen()  
+x = main_menu()
+print(x)
+# game(x[0], x[1], x[2])
+
+
+    
+    
+    
+
+
+
+
+
+
+    
+    
+    
