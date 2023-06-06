3333470d3190fe865563cc945029cc100151fb6f
alra0351 <alexejrausch97@gmail.com>
2023-06-06 10:34:34 +0200

    deleted unused functions

--- Hangman.py
+++ Hangman.py
@@ -6,25 +6,14 @@ def clear_screen():
 
 def main_menu():
     print("(1) Neues Spiel starten")
-    print("(2) Statistiken einsehen")
-    print("(3) Statistiken löschen")
-    print("(4) Beenden")
     user_choice = input()
     if user_choice == "1":
         return new_game()
-    elif user_choice == "2":
-        see_stats()
-    elif user_choice == "3":
-        del_stats()
-    elif user_choice == "4":
-        end_game()
     else:
         print("Wrong Input. Choose again")
         main_menu()
         
-        
-def new_game():
-        
+def new_game():  
     with open("Wortliste.txt", "r") as wordlist:
         lines = wordlist.read().splitlines()
         random_word = random.choice(lines)
@@ -36,7 +25,7 @@ def new_game():
         print(game_string)
         returns = letter_input(random_word, game_string, number_lifes)
         game_string = returns[0]
-        number_lifes = returns[1]        
+        number_lifes = returns[1]       
     if number_lifes > 0:
         clear_screen()
         print("Noch %d Leben übrig" % number_lifes)
@@ -49,22 +38,7 @@ def new_game():
         print(random_word)
         print("Du hast das Spiel verloren")
         return 1, random_word
-
-
-
-def game(random_word, game_string, number_lifes):
-    while( (random_word != game_string) & (number_lifes > 0) ):
-        clear_screen()
-        print("Noch %d Leben übrig" % number_lifes)
-        print(game_string)
-        letter_input(random_word, game_string, number_lifes)
-    if number_lifes > 0:
-        print("Du hast das Spiel gewonnen.")
-    else:
-        print("Du hast das Spiel verloren")
-        return 0
-          
-         
+                
 def letter_input(random_word, game_string, number_lifes):
     letter = input()
     correct_letters = 0
@@ -98,34 +72,8 @@ def print_to_stats_txt(random_word, won_or_lost):
     with open("stats.txt", "w") as f:
         for line in x:
             f.write(line + "\n")    
-    
-def see_stats():
-    return 0
-
-def del_stats():
-    return 0
-
-def end_game():
     return 0
 
 clear_screen()  
 x = main_menu()
 print_to_stats_txt(x[1], x[0])
-
-
-
-
-
-
-    
-    
-    
-
-
-
-
-
-
-    
-    
-    
